90a5f8c23aadd7b449540b872972284dcb24a2ea
Mark Easterly <meeaster@oakland.edu>
2017-03-08 05:03:07 -0500

    stub out authorize methods

--- Client/containers/authorize/authorize.component.ts
+++ Client/containers/authorize/authorize.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { isBrowser } from 'angular2-universal';
-import { Router }    from '@angular/router';
+import { Router, ActivatedRoute }    from '@angular/router';
+import { ApiHttpService } from 'app-shared';
 
 @Component({
     selector: 'app-authorize',
@@ -8,6 +9,42 @@ import { Router }    from '@angular/router';
     styles: [require( './authorize.component.css' )]
 })
 
-export class AuthorizeComponent {
+export class AuthorizeComponent implements OnInit, OnDestroy {
 
+    application: string;
+    requestId: string;
+    scope: string;
+
+    private sub: any;
+
+    constructor (private route: ActivatedRoute, private apiHttpService: ApiHttpService) {
+    
+    }
+
+    authorize() {
+        this.apiHttpService.postForm('/oauth/authorize', { 'request_id': this.requestId, 'submit.Accept': '' })
+            .subscribe(r => {
+
+            }, 
+            error => {
+
+            });
+        console.log('authorize');
+    }
+
+    decline() {
+        console.log('decline');
+    }
+
+    ngOnInit () {
+        this.sub = this.route.queryParams.subscribe(params => {
+            this.application = params['application'];
+            this.requestId = params['request_id'];
+            this.scope = params['scope'];
+        });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
 }
