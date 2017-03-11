79562ad58b60de2b6bcaa4b4cbaadb37670314fd
Brett <barterianb@gmail.com>
2017-03-11 16:00:16 -0500

    update

--- Client/containers/account/account.component.ts
+++ Client/containers/account/account.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }    from '@angular/core';
+import { isBrowser }            from 'angular2-universal';
+
+export class newPass {
+    password:       string;
+    passwordConf:   string;
+}
 
 @Component({
     selector: 'app-account',
     templateUrl: 'account.component.html',
-    styles: ['account.component.css']
+    styleUrls: ['account.component.css']
 })
 export class AccountComponent implements OnInit {
     constructor() { }
