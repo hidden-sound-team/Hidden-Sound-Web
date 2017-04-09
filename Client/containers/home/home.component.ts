d635fda40b98a3e50175be7712b4ebbbfe42655d
Parth <pbpatel092@gmail.com>
2017-04-09 01:43:55 -0400

    Home page update. Still need to work on the wording.

--- Client/containers/home/home.component.ts
+++ Client/containers/home/home.component.ts
@@ -8,6 +8,13 @@ import { isBrowser } from 'angular2-universal';
 })
 export class HomeComponent implements OnInit {
 
+    // Images
+    public logoImageUrl = require('../../images/logo-large.png');
+    public about1 = require('../../images/about1.png');
+    public about2 = require('../../images/about2.png');
+    public howItWorks = require('../../images/how-it-works.png');
+
+
     constructor () {
     }
 
