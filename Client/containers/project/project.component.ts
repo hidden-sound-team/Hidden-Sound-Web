2515951fcaa8673e89f9988606c683db125c21c3
Parth <pbpatel092@gmail.com>
2017-04-30 17:39:36 -0400

    Images

--- Client/containers/project/project.component.ts
+++ Client/containers/project/project.component.ts
@@ -14,6 +14,10 @@ export class ProjectComponent implements OnInit {
     public andrew = require('../../images/team/andrew.png');
     public zane = require('../../images/team/zane.png');
 
+    public buildRelease = require('../../images/info/build-release.png');
+    public createTran = require('../../images/info/create-tran.png');
+    public sysArch = require('../../images/info/sys-arch.png');
+
     constructor() { }
 
     ngOnInit() { }
